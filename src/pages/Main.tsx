4c28740e3bf9aef4f53e4ca6205545f332baf7e3
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-05-29 16:57:10 +0900

    feat: 메인 페이지 스크롤 화면 디자인

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from 'react';
-import styled, { css } from 'styled-components';
+import { useRef } from 'react';
+import styled from 'styled-components';
+import TextBox from '../components/TextBox';
+import { firstDodom } from '../data/textBoxData';
 
 const MainBox = styled.div`
   display: flex;
   flex-direction: column;
   height: calc(-100px + 100vh);
   width: 100vw;
+  margin-bottom: 0px;
 `;
 const Top = styled.div`
   display: flex;
@@ -32,45 +35,87 @@ const ButtonCard = styled.div`
   text-align: center;
 `;
 const Bottom = styled.div`
-  background-color: var(${'--dodom_black'});
+  background-color: var(--black);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   flex-grow: 1;
   h1 {
-    color: var(${'--dodom_yellow'});
+    color: var(--primary);
     margin-bottom: 15px;
   }
   p {
-    color: var(${'--dodom_white'});
+    color: var(--white);
+    cursor: pointer;
   }
 `;
-
+const SubBox = styled.div`
+  position: relative;
+  height: calc(-200px + 100vh);
+  margin-top: -10px;
+  div {
+    padding-bottom: 20px;
+  }
+`;
+const LinkBox = styled.div`
+  position: absolute;
+  bottom: 60px;
+  right: 7rem;
+  display: flex;
+  div {
+    color: var(--white);
+    cursor: pointer;
+    margin-left: 4rem;
+  }
+  img {
+    width: 1rem;
+    margin-left: 1rem;
+  }
+`;
 const Main = () => {
+  const target = useRef<HTMLDivElement>(null);
+  const onMoveToTarget = () => {
+    target?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
   return (
-    <MainBox>
-      <Top>
-        <ButtonCard theme="--dodom_yellow">
-          <img src={process.env.PUBLIC_URL + '/assets/chat.png'} />
-          <h1>솔직한 내 마음 일기 작성하기</h1>
-          <p>아무도 모르는 내 마음, 편하게 써봐요.</p>
-        </ButtonCard>
-        <ButtonCard theme="--dodom_beige">
-          <img src={process.env.PUBLIC_URL + '/assets/glass.png'} />
-          <h1>내 상태 체크하기</h1>
-          <p>
-            내 상태 체크하기는 간단한 설문을 통해 심리 검사를 진행하고,
-            <br />
-            현재 자신의 상태를 알아볼 수 있는 프로그램입니다.
-          </p>
-        </ButtonCard>
-      </Top>
-      <Bottom>
-        <h1>도돔이 처음이라면?</h1>
-        <p>도돔 자세히 알아보기</p>
-      </Bottom>
-    </MainBox>
+    <>
+      <MainBox>
+        <Top>
+          <ButtonCard theme="--primary">
+            <img src={process.env.PUBLIC_URL + '/assets/chat.png'} />
+            <h1>솔직한 내 마음 일기 작성하기</h1>
+            <p>아무도 모르는 내 마음, 편하게 써봐요.</p>
+          </ButtonCard>
+          <ButtonCard theme="--third">
+            <img src={process.env.PUBLIC_URL + '/assets/glass.png'} />
+            <h1>내 상태 체크하기</h1>
+            <p>
+              내 상태 체크하기는 간단한 설문을 통해 심리 검사를 진행하고,
+              <br />
+              현재 자신의 상태를 알아볼 수 있는 프로그램입니다.
+            </p>
+          </ButtonCard>
+        </Top>
+        <Bottom>
+          <h1>도돔이 처음이라면?</h1>
+          <p onClick={onMoveToTarget}>도돔 자세히 알아보기</p>
+        </Bottom>
+      </MainBox>
+      <SubBox ref={target}>
+        <TextBox {...firstDodom} />
+        <LinkBox>
+          <div>
+            상담사 알아보기
+            <img src={process.env.PUBLIC_URL + '/assets/arrow_white.png'} />
+          </div>
+          <div>
+            DODOM 더 알아보기
+            <img src={process.env.PUBLIC_URL + '/assets/arrow_white.png'} />
+          </div>
+        </LinkBox>
+      </SubBox>
+    </>
   );
 };
 
