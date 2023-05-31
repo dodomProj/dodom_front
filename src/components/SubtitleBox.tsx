9f71f31fc832a0c1d431975bb2bc126556670bea
YUNH7 <yunny126@gmail.com>
2023-05-31 16:55:49 +0900

    style: 심리검사세부-서브타이틀 소요시간 디자인 추가

--- src/components/SubtitleBox.tsx
+++ src/components/SubtitleBox.tsx
@@ -32,6 +32,22 @@ export const SubtitleText = styled.div`
   > div {
     position: absolute;
     top: 50%;
+
+    > h1 {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      margin-bottom: 0.5rem;
+    }
+  }
+`;
+const Time = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-weight: normal;
+  > span {
+    font-size: 24px;
   }
 `;
 
@@ -47,8 +63,15 @@ const SubtitleBox = ({
       <img src={img} />
       <SubtitleText>
         <div>
-          <h1>{title}</h1>
-          {time && <span>예상 시간 {time}분</span>}
+          <h1>
+            {title}
+            {time && (
+              <Time>
+                <img src="/assets/time.svg" />
+                <span>예상 시간 {time}분</span>
+              </Time>
+            )}
+          </h1>
           {text?.map((line, idx) => (
             <p key={idx}>{line}</p>
           ))}
