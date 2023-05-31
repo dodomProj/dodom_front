1980ea86dd8036168847f30979b3d18ba7671dcf
YUNH7 <yunny126@gmail.com>
2023-05-31 15:56:01 +0900

    style: 심리검사세부 페이지 구성

--- src/components/AnswerEl.tsx
+++ src/components/AnswerEl.tsx
@@ -1,15 +1,33 @@
+import styled from 'styled-components';
+
 type AnswerProps = {
   img: string;
   text: string;
   score: number;
 };
 
+const Box = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    font-size: 24px;
+  }
+`;
+const AnswerEmoticon = styled.img`
+  width: 100%;
+  max-width: 100%;
+  object-fit: contain;
+  padding: 10%;
+`;
+
 const AnswerEl = ({ img, text, score }: AnswerProps) => {
   return (
-    <li>
-      <img src={img} />
+    <Box>
+      <AnswerEmoticon src={img} />
       <p>{text}</p>
-    </li>
+    </Box>
   );
 };
 
--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 import { Progress } from '@nextui-org/react';
-import AnswerEmoticon from './AnswerEmoticon';
+import Button from './Button';
+import AnswerEl from './AnswerEl';
 import { basePadding } from '../styles/basePadding';
+import { psychologicalQNA } from '../data/psychologicalTest';
+import useFindTest from '../util/useFindTest';
 
 const Box = styled.div`
   ${basePadding}
@@ -11,6 +15,7 @@ const Box = styled.div`
   > p {
     font-size: 24px;
     font-weight: bold;
+    margin-bottom: 1rem;
   }
 `;
 const ProgressStyle = styled(Progress)`
@@ -21,29 +26,57 @@ const ProgressStyle = styled(Progress)`
 `;
 const AnswerBox = styled.ul`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  max-width: 100%;
+  justify-content: space-around;
+  align-items: start;
   list-style: none;
 `;
 const ButtonBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 1rem;
 `;
 
 const QuestionBox = () => {
+  const testQNA = useFindTest(psychologicalQNA);
+
+  const [questionIdx, setQuestionIdx] = useState<number>(0);
+  const [questionsLen, setQuestionsLen] = useState<number>(0);
+
+  useEffect(() => {
+    if (testQNA?.questions) {
+      setQuestionsLen(testQNA.questions.length);
+    }
+  }, [testQNA]);
+
   return (
     <Box>
-      <p>1. 아침에 일어나는게 두렵다.</p>
-      <ProgressStyle value={50} size="sm" color="warning" status="warning" />
+      <p>{testQNA?.questions && testQNA?.questions[questionIdx]}</p>
+      <ProgressStyle
+        value={((questionIdx + 1) / questionsLen) * 100}
+        size="sm"
+        color="warning"
+        status="warning"
+      />
       <AnswerBox>
-        <AnswerEmoticon />
-        <AnswerEmoticon />
-        <AnswerEmoticon />
+        {testQNA?.answers?.map((answer, idx) => (
+          <AnswerEl key={idx} {...answer} />
+        ))}
       </AnswerBox>
       <ButtonBox>
-        <button>이전</button>
-        <button>다음</button>
+        <Button
+          text="이전"
+          onClick={() => questionIdx > 0 && setQuestionIdx(questionIdx - 1)}
+        />
+        <Button
+          text={questionIdx === questionsLen - 1 ? '완료' : '다음'}
+          onClick={() =>
+            questionsLen &&
+            questionIdx < questionsLen - 1 &&
+            setQuestionIdx(questionIdx + 1)
+          }
+        />
       </ButtonBox>
     </Box>
   );
