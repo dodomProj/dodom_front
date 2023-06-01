94bde25cba0dc038417587281896b032c2091550
YUNH7 <yunny126@gmail.com>
2023-06-01 10:59:02 +0900

    style: 정책 페이지 다이어리 배너 추가

--- src/pages/Policy.tsx
+++ src/pages/Policy.tsx
@@ -1,10 +1,13 @@
 import PolicySubtitle from '../components/PolicySubtitle';
+import ToDiary from '../components/ToDiary';
 import { PageBase } from '../styles/basePadding';
 
 const Policy = () => {
   return (
     <PageBase>
       <PolicySubtitle />
+      <p>hi</p>
+      <ToDiary />
     </PageBase>
   );
 };
