1d3a93bf33d9de9f8704ff92d344d510b663897f
YUNH7 <yunny126@gmail.com>
2023-06-05 16:58:16 +0900

    style: 이미지 크기 조정, 소요시간 추가

--- src/components/TestCard.tsx
+++ src/components/TestCard.tsx
@@ -1,34 +1,49 @@
 import { FC } from 'react';
-import { TestInfo } from '../data/psychologicalTest';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { TestInfo } from '../data/psychologicalTest';
 
 interface TestCardInfo {
   test: TestInfo;
 }
 
-const Card = styled.article`
-  height: 600px;
+const Card = styled.div`
+  height: 100%;
   background: var(--secondary);
   box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
-  padding: 20px;
+  padding: 2.5rem;
+
+  > img {
+    width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+    /* padding: 1rem; */
+  }
 
   > h3 {
-    font-size: 24px;
+    font-size: 1.5rem;
     font-weight: normal;
     margin: 0.5rem 0;
   }
 `;
+const Time = styled.p`
+  font-size: 0.75rem;
+  align-self: end;
+  text-align: end;
+`;
 
 const TestCard: FC<TestCardInfo> = ({ test }) => {
-  const { img, title, text } = test;
+  const { key, img, title, text, time } = test;
+  const navigate = useNavigate();
   return (
-    <Card className="slider-item">
-      <img src={img} />
+    <Card onClick={() => navigate(`/condition/${key}`)}>
+      <img src={img} alt="" />
       <h3>{title}</h3>
       {text.map((line, idx) => (
         <p key={idx}>{line}</p>
       ))}
+      <Time>예상시간 {time}분</Time>
     </Card>
   );
 };
