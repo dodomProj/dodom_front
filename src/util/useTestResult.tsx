99da018b114d1a3ec5a65e46f6e2d4d2dbfced1e
YUNH7 <yunny126@gmail.com>
2023-06-10 18:20:52 +0900

    fix: 리턴 데이터 수정

--- src/util/useTestResult.tsx
+++ src/util/useTestResult.tsx
@@ -1,24 +1,36 @@
 import { useEffect, useState } from 'react';
-import { TestResultInfo, psychologicalResult } from '../data/psychologicalTest';
+import {
+  ResultCommonInfo,
+  ResultTypeInfo,
+  TestResultInfo,
+  psychologicalResult,
+} from '../data/psychologicalTest';
 import useFindTest from './useFindTest';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { answerScoreState, answersState } from '../recoil';
 
-const useTestResult = () => {
-  const [answers, setAnswers] = useRecoilState(answersState);
-  const [answerScore, setAnswerScore] = useRecoilState(answerScoreState);
+export interface TestResult {
+  score: number;
+  scoreArr: number[];
+  common?: ResultCommonInfo;
+  type?: ResultTypeInfo;
+}
+
+const useTestResult = (): TestResult => {
+  const answers = useRecoilValue(answersState);
+  const answerScore = useRecoilValue(answerScoreState);
   const resultObj = useFindTest<TestResultInfo>(psychologicalResult);
 
-  const [score, setScore] = useState<number | number[]>(0);
+  const [score, setScore] = useState<number>(0);
+  const [scoreArr, setScoreArr] = useState<number[]>([]);
   const [type, setType] = useState(resultObj?.result[0]);
 
   useEffect(() => {
     if (resultObj?.result) {
-      let tmpScore: number | number[] = 0;
       let tmpType;
 
       if (Array.isArray(resultObj.result[0].score)) {
-        tmpScore = resultObj.result.map((type) =>
+        let tmpScoreArr = resultObj.result.map((type) =>
           Array.isArray(type.score)
             ? type.score.reduce(
                 (score, questionNum) =>
@@ -27,27 +39,27 @@ const useTestResult = () => {
               )
             : 0
         );
-        tmpScore.pop();
-        const max = Math.max(...tmpScore);
+        tmpScoreArr.pop();
+        setScoreArr(tmpScoreArr);
+        const max = Math.max(...tmpScoreArr);
         tmpType =
-          tmpScore.filter((score) => score === max).length === tmpScore.length
+          tmpScoreArr.filter((score) => score === max).length ===
+          tmpScoreArr.length
             ? resultObj.result[resultObj.result.length - 1]
-            : resultObj.result[tmpScore.indexOf(max)];
+            : resultObj.result[tmpScoreArr.indexOf(max)];
       } else {
-        tmpScore = answers.reduce(
+        let tmpScore = answers.reduce(
           (score, answerIdx) => score + answerScore[answerIdx],
           0
         );
+        setScore(tmpScore);
         tmpType = resultObj.result.find((el) => tmpScore <= el.score);
       }
-      setScore(tmpScore);
       setType(tmpType);
-      setAnswers([]);
-      setAnswerScore([]);
     }
-  }, [resultObj]);
+  }, [resultObj, answers, answerScore]);
 
-  return { score, common: resultObj?.common, type };
+  return { score, scoreArr, common: resultObj?.common, type };
 };
 
 export default useTestResult;
