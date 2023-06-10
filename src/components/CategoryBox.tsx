9722fd70b81226871a1ad2a1392c19e8d4b5b0a9
YUNH7 <yunny126@gmail.com>
2023-06-10 23:43:07 +0900

    style: 텍스트정렬, 아래마진(+기본값) 추가

--- src/components/CategoryBox.tsx
+++ src/components/CategoryBox.tsx
@@ -3,27 +3,40 @@ import styled from 'styled-components';
 interface CategoryProps {
   title: string;
   text: string;
-  textsize: string;
+  textsize?: string;
+  textalign?: string;
+  marginbottom?: string;
 }
 interface TextStyle {
-  textfs: string;
+  textsize?: string;
+  textalign?: string;
+  marginbottom?: string;
 }
 
 const Text = styled.div<TextStyle>`
+  text-align: ${(props) => props.textalign || ''};
+  margin-bottom: ${(props) => props.marginbottom || '0'};
+
   > h2 {
     margin-bottom: 0.5rem;
   }
 
   > p {
-    font-size: ${(props) => props.textfs};
+    font-size: ${(props) => props.textsize || '1.2rem'};
     line-height: 2rem;
     white-space: pre-line;
   }
 `;
 
-const CategoryBox = ({ title, text, textsize }: CategoryProps) => {
+const CategoryBox = ({
+  title,
+  text,
+  textsize,
+  textalign,
+  marginbottom,
+}: CategoryProps) => {
   return (
-    <Text textfs={textsize}>
+    <Text textsize={textsize} textalign={textalign} marginbottom={marginbottom}>
       <h2>{title}</h2>
       <p>{text}</p>
     </Text>
