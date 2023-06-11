0ab571d5bc875db99e1e1b23dea5f80287ffc4f9
YUNH7 <yunny126@gmail.com>
2023-06-11 20:38:54 +0900

    style: 스트레스 대처 결과(차트 생성 전)

--- src/components/condition/ConditionResultContents.tsx
+++ src/components/condition/ConditionResultContents.tsx
@@ -1,10 +1,13 @@
 import { ResultCardProps } from '../ConditionResultCard';
+import BasicResult from './BasicResult';
 import CommunicationResult from './CommunicationResult';
+import StressManageResult from './StressManageResult';
+import StressResult from './StressResult';
 
 const ConditionResultContents = ({ test }: ResultCardProps) => {
-  if (test === 'anxiety' || test === 'depression') return null;
-  if (test === 'stress') return null;
-  if (test === 'stress-management') return null;
+  if (test === 'anxiety' || test === 'depression') return <BasicResult />;
+  if (test === 'stress') return <StressResult />;
+  if (test === 'stress-management') return <StressManageResult />;
   if (test === 'communication-parent-child') return <CommunicationResult />;
   return null;
 };
