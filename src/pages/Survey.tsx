a86570f70afa6c7defa636083f944e76b4fb00ef
YUNH7 <yunny126@gmail.com>
2023-06-12 10:08:14 +0900

    style: 설문 문항 변경

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -47,24 +47,23 @@ const Survey = () => {
         <QuestionInput
           question="1. 본인의 이름을 작성해주세요."
           type="text"
-          placeholder="실제 상담 후 진행된 설문인지 확인 시에 시용됩니다. 이외의 어느 곳에도 이용되지 않습니다."
+          placeholder="김도돔"
           textareaRows={5}
         />
-        <QuestionInput question="2. 담당 상담사를 선택해주세요." />
-        <QuestionInput question="3. 상담은 어느 정도 만족하셨나요?" />
+        <QuestionInput question="2. 상담은 어느 정도 만족하셨나요?" />
         <QuestionInput
-          question="4. 상담의 어느 부분에서 만족/불만족 하셨나요? 자유롭게 작성해주세요."
+          question="3. 상담의 어느 부분에서 만족/불만족 하셨나요? 자유롭게 작성해주세요."
           type="textarea"
-          placeholder="위 후기는 익명으로 도돔의 상담사 후기란 혹은 도돔 소개 게시판에 게시될 가능성이 있습니다.&#13;여러분의 후기는 다른 이들에게 또 다른 용기가 됩니다."
+          placeholder="ex. 현재 고민을 잘 들어주셨고, 실질적인 대처 방식을 제시해줘서 좋았어요.&#13;도돔 서비스의 연락 방식이나, 시간 약속 시스템이 만족스러웠어요."
           textareaRows={5}
         />
         <QuestionInput
-          question="5. 상담사에게 하고싶은 말을 자유롭게 작성해주세요."
+          question="4. 상담사에게 하고싶은 말을 자유롭게 작성해주세요."
           type="textarea"
-          placeholder="위 후기는 익명으로 도돔의 상담사 후기란에 게시될 가능성이 있습니다. 여러분의 후기는 다른 이들에게 또 다른 용기가 됩니다."
+          placeholder="ex. 도움이 된 점, 앞으로 달라지고 싶은 내 모습 등."
           textareaRows={5}
         />
-        <Button text="확인" onClick={() => console.log('버튼 클릭')} />
+        <Button text="제출하기" onClick={() => console.log('버튼 클릭')} />
       </SurveyBox>
     </PageBase>
   );
