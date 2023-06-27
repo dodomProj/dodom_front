868c28028d48b2339d99aedb9df8145146a945fe
YUNH7 <yunny126@gmail.com>
2023-06-27 11:45:42 +0900

    data: 정책, 에러, 리뷰 타이틀

--- src/data/categoryBoxData.tsx
+++
@@ -1,9 +0,0 @@
-export interface CategoryDataProps {
-  title: string;
-  text: string;
-}
-
-export const recommended: CategoryDataProps = {
-  title: '추천 상담사',
-  text: `적합한 상담사를 추천해드려요!\n상담사를 클릭하면, 더 자세히 알아볼 수 있어요.`,
-};
