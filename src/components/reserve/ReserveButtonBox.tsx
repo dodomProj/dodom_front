ee84f5ec8ad16f53108c03b4a5997204f6220a4e
YUNH7 <yunny126@gmail.com>
2023-06-30 01:14:02 +0900

    feat: 상담예약, 로딩, 에러 처리

--- src/components/reserve/ReserveButtonBox.tsx
+++ src/components/reserve/ReserveButtonBox.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import {
+  useRecoilState,
+  useRecoilValue,
+  useRecoilValueLoadable,
+  useResetRecoilState,
+} from 'recoil';
 import Button from '../Button';
+import Loading from '../Loading';
 import postData from '../../api/postData';
 import { selectedCounselorsState } from '../../recoil/counsel';
-import { formDataState, recommendedsState } from '../../recoil/reserve';
+import {
+  FormData,
+  formDataState,
+  recommendedsState,
+} from '../../recoil/reserve';
 
 const ButtonBox = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -17,29 +30,60 @@ const ButtonBox = styled.div`
     padding-right: 2rem;
   }
 `;
+const LoadingWrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(17, 14, 13, 0.4);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+`;
 
 const ReserveButtonBox = () => {
   const selectedCounselors = useRecoilValue(selectedCounselorsState);
   const [formData, setFormData] = useRecoilState(formDataState);
+  const resetFormData = useResetRecoilState(formDataState);
   const { state, contents: recommendeds } =
     useRecoilValueLoadable(recommendedsState);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const postReserve = (form: FormData) => {
+    setIsLoading(true);
+    postData('/appointments', form).then((res) =>
+      res.status === 201
+        ? (resetFormData(), navigate('/reserve/reserved'))
+        : (alert('예약이 실패했습니다.\n잠시 후 다시 요청해주세요'),
+          navigate('/'))
+    );
+  };
 
   const SubmitReserveForm = () => {
     if (formData.counselorId === -1) {
       const counselors = [...new Set(selectedCounselors)];
       const numOfCounselors = counselors.length;
       if (numOfCounselors === 1)
-        return postData('/appointments', {
+        postReserve({
           ...formData,
           counselorId: counselors[0],
         });
       else alert('상담사 한 명을 선택해주세요');
     } else if (Object.values(formData).every((value) => value))
-      postData('/appointments', formData);
+      postReserve(formData);
   };
 
   return (
     <ButtonBox>
+      {isLoading && (
+        <LoadingWrapper>
+          <Loading />
+        </LoadingWrapper>
+      )}
       <Button
         white={true}
         text="랜덤 배정받기"
