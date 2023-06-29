7d344beace813576771e54af967c4d2f586794d4
YUNH7 <yunny126@gmail.com>
2023-06-29 19:09:46 +0900

    fix: reserve 타이틀 전역상태 받는 위치 변경(App > Reserve)

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,6 @@ import Survey from './pages/Survey';
 import Error from './pages/Error';
 import postData from './api/postData';
 
-import { useRecoilValue } from 'recoil';
-import { reserveCardInfo } from './recoil/reserve';
-
 function App() {
   const { pathname } = useLocation();
   const [visitCount, setVisitCount] = useState(0);
@@ -31,8 +28,6 @@ function App() {
     );
   }, []);
 
-  const info = useRecoilValue(reserveCardInfo);
-
   return (
     <>
       <GlobalStyle />
@@ -41,7 +36,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/diary/*" element={<Diary />} />
         <Route path="/reserve/counsel" element={<Counsel />} />
-        <Route path="/reserve/*" element={<Reserve {...info} />} />
+        <Route path="/reserve/*" element={<Reserve />} />
         <Route path="/counsel/*" element={<Counsel />} />
         <Route path="/condition" element={<Condition />} />
         <Route path="/condition/*" element={<ConditionCheck />} />
